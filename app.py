704690e709aa9b0a31bf5d0b43578d05a75c49eb
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-04-26 00:19:36 +0530

    initial

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
 from flask import Flask
+
+from db import db_cursor
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/list-customers')
+def list_customers():  # put application's code here
+    db_cursor.execute("SELECT * FROM customer")
+    records = db_cursor.fetchall()
+    return {"data": records}
 
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+# Connect to your postgres DB
+conn = psycopg2.connect(dbname="postgres", user="user", password="password", host="temp.devmrfitz.xyz", port="7254")
+
+# Open a cursor to perform database operations
+db_cursor = conn.cursor()
+#
+# # Execute a query
+# db_cursor.execute("SELECT * FROM customer")
+#
+# # Retrieve query results
+# records = db_cursor.fetchall()
+#
+# print(records)
+#
+#
+# # Execute a query
+# db_cursor.execute("SELECT * FROM customer where id = 1")
+#
+# # Retrieve query results
+# records = db_cursor.fetchall()
+#
+# print(records)
