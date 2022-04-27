e237cec23f4bef46baf8ab889316e65c6d0e0adf
mishraanis <mishraanis@gmail.com>
2022-04-28 01:16:29 +0530

    ticket making query

--- app.py
+++ app.py
@@ -296,6 +296,15 @@ def create_plan(db_cursor, db_connection, username):
     else:
         return {"message": "Plan created successfully."}
 
+@app.route('/api/ticket/create', methods=['POST'])
+@login_required
+def create_ticket(db_cursor, db_connection, username):
+    db_cursor.execute(f"INSERT INTO {username}ticket (timestamp , status, resolver, raiser) VALUES ('{datetime.datetime.now()}', false , null , SELECT owner FROM phone WHERE mobile_number = {request.json('ticket_mobile_number')})")
+    db_connection.commit()
+    if db_cursor.rowcount == 0:
+        return {"message": "Ticket not created."}
+    else:
+        return {"message": "Ticket created successfully."}
 
 @app.route('/api/tickets/list')
 @login_required
